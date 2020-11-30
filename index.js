3223868df20f3ab7f4536ca9c076542222871361
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-11-30 15:09:19 +0800

    add commitlint

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const prettier = require('./prettier');
 const eslint = require('./eslint');
 const stylelint = require('./stylelint');
+const commitlint = require('./commitlint');
 
 module.exports = {
   prettier,
   eslint,
   stylelint,
+  commitlint,
 };
