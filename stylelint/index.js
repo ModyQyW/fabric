1571cdadfd630e0cbc01d42ee47b13bb5b0e84d9
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-02-02 15:10:07 +0800

    feat: add stylelint/sass config

--- stylelint/index.js
+++ stylelint/index.js
@@ -1,9 +1,11 @@
 const css = require('./css');
 const less = require('./less');
+const sass = require('./sass');
 const scss = require('./scss');
 
 module.exports = {
   css,
   less,
+  sass,
   scss,
 };
