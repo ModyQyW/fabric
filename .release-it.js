71a1365c04e83327a7bb97b398338ef432f02532
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-07-19 19:11:45 +0800

    chore: update script

--- .release-it.js
+++ .release-it.js
@@ -3,7 +3,7 @@ module.exports = {
     commitMessage: 'chore(release): v${version}',
   },
   hooks: {
-    'before:init': 'yarn run test',
+    'before:init': 'yarn run lint',
     'after:bump': 'node generate-readme.js',
   },
 };
