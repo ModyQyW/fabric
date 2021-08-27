757767b3f70c001d8a5560723df489c4eec6fd44
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-08-27 20:44:14 +0800

    chore: specify release-it config

--- .release-it.js
+++ .release-it.js
@@ -1,6 +1,12 @@
 module.exports = {
+  npm: {
+    push: true,
+  },
   git: {
+    commit: true,
     commitMessage: 'chore(release): v${version}',
+    push: true,
+    tag: true,
   },
   hooks: {
     'before:init': 'pnpm install && pnpm run lint',
