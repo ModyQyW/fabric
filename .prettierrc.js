b030131ead77743bd7c34b91963f9785419bd7d7
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-08-28 13:13:03 +0800

    chore: use dist config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,20 +1,5 @@
+const { prettier } = require('./dist/index.cjs');
+
 module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
-  singleQuote: true,
-  quoteProps: 'as-needed',
-  jsxSingleQuote: false,
-  trailingComma: 'all',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  arrowParens: 'always',
-  requirePragma: false,
-  insertPragma: false,
-  proseWrap: 'preserve',
-  htmlWhitespaceSensitivity: 'css',
-  vueIndentScriptAndStyle: false,
-  endOfLine: 'lf',
-  embeddedLanguageFormatting: 'auto',
+  ...prettier,
 };
