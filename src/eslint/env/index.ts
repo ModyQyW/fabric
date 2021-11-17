8ccaac0b794144e35b94db75fa779ca0812f08ad
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-11-17 09:43:12 +0800

    feat: add more env

--- src/eslint/env/index.ts
+++ src/eslint/env/index.ts
@@ -1,9 +1,13 @@
 const env: Record<string, boolean> = {
   browser: true,
+  commonjs: true,
   es2021: true,
+  jasmine: true,
   jest: true,
   mocha: true,
   node: true,
+  shelljs: true,
+  mongo: true,
 };
 
 export default env;
