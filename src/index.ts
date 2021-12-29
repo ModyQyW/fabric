bfd26a33c6897db0bf6fb427b7987fe8e5ba5ca3
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-12-29 14:47:20 +0800

    fix: try fix export

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,8 @@ export default {
   prettier,
   stylelint,
 };
+
+export { default as commitlint } from './commitlint';
+export { default as prettier } from './prettier';
+export { default as eslint } from './eslint';
+export { default as stylelint } from './stylelint';
