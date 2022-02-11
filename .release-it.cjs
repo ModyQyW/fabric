a6070541ae35df28ed1543986173e475f8f51592
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-02-11 17:40:53 +0800

    chore: update release-it config

--- .release-it.cjs
+++ .release-it.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   git: {
     commitMessage: 'chore(release): v${version}',
+    tagName: 'v${version}',
   },
   hooks: {
     'before:init': 'pnpm install && pnpm run build && pnpm run lint',
