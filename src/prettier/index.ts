40ff6fac8d63ed07c25eda43c67dae071adc172b
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-03-16 15:04:38 +0800

    feat: prettier add singleAttributePerLine

--- src/prettier/index.ts
+++ src/prettier/index.ts
@@ -1,24 +1,10 @@
 import type { Config } from 'prettier';
 
-const config: Config = {
+const config: Config & { singleAttributePerLine?: boolean } = {
   printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
   singleQuote: true,
-  quoteProps: 'as-needed',
-  jsxSingleQuote: false,
   trailingComma: 'all',
-  bracketSpacing: true,
-  bracketSameLine: false,
-  arrowParens: 'always',
-  requirePragma: false,
-  insertPragma: false,
-  proseWrap: 'preserve',
-  htmlWhitespaceSensitivity: 'css',
-  vueIndentScriptAndStyle: false,
-  endOfLine: 'lf',
-  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: true,
   plugins: [
     './node_modules/prettier-plugin-jsdoc',
     './node_modules/prettier-plugin-packagejson',
