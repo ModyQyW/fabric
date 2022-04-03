c08bd21c836f99ca92062d4f688d72e6c41784d1
ModyQyW <wurui-dev@formail.com>
2022-04-03 21:32:12 +0800

    fix: fix vanilla-typescript

--- src/eslint/rules/vanilla-typescript.ts
+++ src/eslint/rules/vanilla-typescript.ts
@@ -3,7 +3,26 @@ import type { Linter } from 'eslint';
 
 const rules: Partial<Record<string, Linter.RuleLevel | Linter.RuleLevelAndOptions>> = {
   ...vanilla,
+  'constructor-super': 'off',
+  'getter-return': 'off',
+  'no-const-assign': 'off',
+  'no-dupe-args': 'off',
+  'no-dupe-class-members': 'off',
+  'no-dupe-keys': 'off',
+  'no-func-assign': 'off',
+  'no-import-assign': 'off',
+  'no-new-symbol': 'off',
+  'no-obj-calls': 'off',
+  'no-redeclare': 'off',
+  'no-setter-return': 'off',
+  'no-this-before-super': 'off',
   'no-undef': 'off',
+  'no-unreachable': 'off',
+  'no-unsafe-negation': 'off',
+  'valid-typeof': 'off',
+  'import/named': 'off',
+  'import/no-named-as-default-member': 'off',
+  'import/no-unresolved': ['error', { commonjs: true, caseSensitive: true }],
 };
 
 export default rules;
