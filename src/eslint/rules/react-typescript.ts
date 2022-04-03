6efa81af1b6c3712af5e79289ff4ec19f821d51c
ModyQyW <wurui-dev@formail.com>
2022-04-03 21:32:31 +0800

    feat: remove react/jsx-filename-extension

--- src/eslint/rules/react-typescript.ts
+++ src/eslint/rules/react-typescript.ts
@@ -3,7 +3,6 @@ import type { Linter } from 'eslint';
 
 const rules: Partial<Record<string, Linter.RuleLevel | Linter.RuleLevelAndOptions>> = {
   ...react,
-  'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
 };
 
 export default rules;
