51be4efb050ad9b6d092b8fff740d55ba229d9b3
ModyQyW <wurui-dev@formail.com>
2022-04-03 20:53:39 +0800

    fix: move react/jsx-filename-extension

--- src/eslint/rules/react-typescript.ts
+++ src/eslint/rules/react-typescript.ts
@@ -3,6 +3,7 @@ import type { Linter } from 'eslint';
 
 const rules: Partial<Record<string, Linter.RuleLevel | Linter.RuleLevelAndOptions>> = {
   ...react,
+  'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
 };
 
 export default rules;
