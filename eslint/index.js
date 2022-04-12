da963a6d03249cca98545c4d901147d6f357b2cb
ModyQyW <wurui-dev@formail.com>
2022-04-12 08:35:39 +0800

    feat: update eslint config export

--- eslint/index.js
+++ eslint/index.js
@@ -1,17 +1,21 @@
+const reactMiniprogram = require('./react-miniprogram');
 const react = require('./react');
 const vanilla = require('./vanilla');
 const svelteTypescript = require('./svelte-typescript');
 const svelte = require('./svelte');
+const vueMiniprogram = require('./vue-miniprogram');
 const vueTypescript = require('./vue-typescript');
 const vue = require('./vue');
 const vue2Typescript = require('./vue2-typescript');
 const vue2 = require('./vue2');
 
 module.exports = {
+  reactMiniprogram,
   react,
   vanilla,
   svelteTypescript,
   svelte,
+  vueMiniprogram,
   vueTypescript,
   vue,
   vue2Typescript,
