ca1e861fe2c2a39d730aa2705e264b6b062e7711
ModyQyW <wurui-dev@foxmail.com>
2022-05-07 20:56:23 +0800

    chore: update release-it

--- .release-it.cjs
+++ .release-it.cjs
@@ -3,6 +3,13 @@ module.exports = {
     commitMessage: 'chore(release): v${version}',
     tagName: 'v${version}',
   },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      header: '# Changelog',
+      infile: 'CHANGELOG.md',
+      preset: 'conventionalcommits',
+    },
+  },
   hooks: {
     'before:init': 'pnpm install && pnpm run lint',
   },
