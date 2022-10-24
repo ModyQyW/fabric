3654cbb910df0f1727380bf3cfa0b5b763172c36
ModyQyW <wurui-dev@foxmail.com>
2022-10-24 18:36:21 +0800

    chore: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,29 +1,22 @@
-/* eslint-disable no-useless-escape */
 import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
     entry: ['src/**/*.ts'],
-    format: 'esm',
+    clean: true,
+    format: ['esm', 'cjs'],
     dts: true,
     minify: true,
     shims: true,
     splitting: false,
     target: 'node12',
-    banner: {
-      js: `import {createRequire as __createRequire} from 'module';var require=createRequire(import\.meta.url);`,
-    },
-  },
-  {
-    entry: ['src/**/*.ts'],
-    format: 'cjs',
-    minify: true,
-    shims: true,
-    splitting: false,
-    target: 'node12',
-    footer: {
-      js: `if (module.exports.default) module.exports = module.exports.default;`,
+    footer: ({ format }) => {
+      if (format === 'cjs') {
+        return {
+          js: `if (module.exports.default) module.exports = module.exports.default;`,
+        };
+      }
+      return;
     },
   },
 ]);
-/* eslint-enable no-useless-escape */
