e5609e1108d25bbfda6c96a5126e25a0d514c2d5
ModyQyW <wurui-dev@foxmail.com>
2022-10-31 18:54:25 +0800

    perf: remove duplicate configs

--- src/stylelint/scss.ts
+++ src/stylelint/scss.ts
@@ -4,15 +4,12 @@ export const config: Stylelint.Config = {
   extends: ['stylelint-config-recommended-scss', 'stylelint-config-prettier-scss'],
   rules: {
     // tailwindcss
-    'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: ['tailwind', 'value', 'layer', 'config'],
       },
     ],
-    // scss
-    'annotation-no-unknown': [true, { ignoreAnnotations: 'default' }],
   },
 };
 
