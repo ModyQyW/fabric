fe33d807f5488bf8572ac0712270713baf9c97ac
ModyQyW <wurui-dev@foxmail.com>
2023-01-28 20:51:00 +0800

    feat: update prettier

--- src/prettier/index.ts
+++ src/prettier/index.ts
@@ -1,6 +1,7 @@
-import type * as Prettier from 'prettier';
+/* eslint-disable unicorn/prefer-module */
+import type { Config } from 'prettier';
 
-export const config: Prettier.Config = {
+const config: Config = {
   printWidth: 100,
   singleQuote: true,
   trailingComma: 'all',
@@ -9,6 +10,16 @@ export const config: Prettier.Config = {
     require.resolve('prettier-plugin-packagejson'),
     require.resolve('prettier-plugin-tailwindcss'),
   ],
+  overrides: [
+    {
+      files: ['*.json5'],
+      options: {
+        singleQuote: false,
+        quoteProps: 'preserve',
+      },
+    },
+  ],
 };
 
 export default config;
+/* eslint-enable unicorn/prefer-module */
