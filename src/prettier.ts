6b30c288af1600e4b86a69a7a23a2b1d9038c8cc
ModyQyW <wurui-dev@foxmail.com>
2023-01-31 10:49:26 +0800

    fix: fix prettier config

--- src/prettier.ts
+++ src/prettier.ts
@@ -1,10 +1,12 @@
 /* eslint-disable unicorn/prefer-module */
 import type { Config } from 'prettier';
 
-const config: Config = {
+const config: Config & { [key: string]: any } = {
   printWidth: 100,
   singleQuote: true,
   trailingComma: 'all',
+  jsdocCapitalizeDescription: false,
+  tsdoc: true,
   plugins: [
     require.resolve('prettier-plugin-jsdoc'),
     require.resolve('prettier-plugin-packagejson'),
