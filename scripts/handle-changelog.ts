e22e0905b32e6d7b7c2559789bbfefe2565001a7
ModyQyW <wurui-dev@foxmail.com>
2023-02-20 17:38:14 +0800

    chore: handle changelog

--- scripts/handle-changelog.ts
+++ scripts/handle-changelog.ts
@@ -1,4 +1,5 @@
 import { readFileSync, writeFileSync } from 'node:fs';
+import { simpleGit } from 'simple-git';
 
 const filePath = 'CHANGELOG.md';
 
@@ -6,3 +7,5 @@ const content = readFileSync(filePath, { encoding: 'utf8' });
 if (!content.startsWith('# Changelog')) {
   writeFileSync(filePath, `# Changelog\n\n${content}`);
 }
+
+simpleGit().add(filePath);
