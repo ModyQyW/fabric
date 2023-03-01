e49ec0fd006fbc4f88879cebbb9fcc3f33b71c33
ModyQyW <wurui-dev@foxmail.com>
2023-03-01 18:15:56 +0800

    chore: sort package.json when commit

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  'package.json': "sort-package-json",
   '*.md': 'markdownlint --fix --ignore=CHANGELOG.md --ignore-path=.gitignore',
   '*.{js,cjs,mjs,ts,cts,mts}': 'eslint --fix --cache',
 };
