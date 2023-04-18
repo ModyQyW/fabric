044407b0044d9ca3b5873c025e6addc26f82eda2
ModyQyW <wurui-dev@foxmail.com>
2023-04-18 22:00:16 +0800

    chore: update exports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ['./dist/eslint'],
+  extends: ['./dist/eslint.cjs'],
 };
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,3 @@
 module.exports = {
-  ...require('./dist/prettier'),
+  ...require('./dist/prettier.cjs'),
 };
