008775484186ad75c774343d4ecc69300f24a94e
ModyQyW <wurui-dev@foxmail.com>
2023-05-07 17:08:46 +0800

    build!: target node16

--- build.config.ts
+++ build.config.ts
@@ -4,11 +4,10 @@ export default defineBuildConfig({
   entries: ['./src/index', './src/eslint', './src/prettier', './src/stylelint'],
   clean: true,
   declaration: true,
-  failOnWarn: false,
   rollup: {
     emitCJS: true,
     esbuild: {
-      target: 'node14.18',
+      target: 'node16',
     },
   },
 });
