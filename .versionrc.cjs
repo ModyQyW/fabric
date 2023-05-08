6c782105dbdebc8f490b5f2f3f497691416aa842
ModyQyW <wurui-dev@foxmail.com>
2023-05-08 20:07:55 +0800

    chore: config

--- .versionrc.cjs
+++ .versionrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   scripts: {
-    prerelease: "concurrently \"pnpm:lint\" \"pnpm:check:types\"",
+    prerelease: "conc \"pnpm:lint\" \"pnpm:check:types\"",
     posttag: "git push --follow-tags"
   },
 };
