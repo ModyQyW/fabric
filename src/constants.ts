6bdf5ca59a6f0b4021a1ba766ae66cbe37eeb727
ModyQyW <wurui-dev@foxmail.com>
2023-12-08 21:23:09 +0800

    feat: add constants

--- src/constants.ts
+++ src/constants.ts
@@ -18,6 +18,8 @@ export const GLOB_JSONC = '**/*.jsonc';
 
 export const GLOB_MARKDOWN = '**/*.md';
 export const GLOB_VUE = '**/*.vue';
+export const GLOB_SVELTE = '**/*.svelte';
+export const GLOB_ASTRO = '**/*.astro';
 export const GLOB_YAML = '**/*.y?(a)ml';
 export const GLOB_HTML = '**/*.htm?(l)';
 
