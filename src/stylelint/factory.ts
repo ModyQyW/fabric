0ec2d34d0029fc8ad5f3ea611f54ece88e4ae301
ModyQyW <wurui.dev@gmail.com>
2024-03-14 10:08:04 +0800

    feat(stylelint): add stylelint-high-performance-animation

--- src/stylelint/factory.ts
+++ src/stylelint/factory.ts
@@ -11,16 +11,19 @@ export function stylelint(
   return {
     extends: extends_(parsed),
     ignoreFiles: GLOB_EXCLUDE,
-    overrides: [
-      {
-        customSyntax: 'postcss-html',
-        files: ['*.vue', '**/*.vue'],
-      },
+    plugins: [
+      'stylelint-high-performance-animation',
+      // 'stylelint-plugin-defensive-css',
+      // 'stylelint-plugin-logical-css',
     ],
     reportDescriptionlessDisables: true,
     reportInvalidScopeDisables: true,
     reportNeedlessDisables: true,
-    rules: rules(parsed),
+    rules: {
+      ...rules(parsed),
+      // stylelint-high-performance-animation
+      'plugin/no-low-performance-animation-properties': true,
+    },
     ...userConfig,
   };
 }
