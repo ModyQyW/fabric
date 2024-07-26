9bf74525b4b361505ffaa0955676c469869390bb
Rui Wu <wurui.dev@gmail.com>
2024-07-26 10:15:27 +0800

    feat(eslint): update prettier config
    We can remove eslint-plugin-prettier and eslint-config-prettier now.

--- src/eslint/plugins.ts
+++ src/eslint/plugins.ts
@@ -1,110 +1,72 @@
-import { interopDefault } from '../utils';
-
-// handle below
-
 import * as _configGitignore from 'eslint-config-flat-gitignore';
-export const configGitignore = interopDefault(_configGitignore);
-
 import * as _pluginImportX from 'eslint-plugin-import-x';
-export const pluginImportX = interopDefault(_pluginImportX);
-
 import * as _pluginJsdoc from 'eslint-plugin-jsdoc';
-export const pluginJsdoc = interopDefault(_pluginJsdoc);
-
 import * as _pluginJsonc from 'eslint-plugin-jsonc';
-export const pluginJsonc = interopDefault(_pluginJsonc);
-
 /* @ts-expect-error missing types */
 import * as _pluginMarkdown from 'eslint-plugin-markdown';
-export const pluginMarkdown = interopDefault(_pluginMarkdown);
-
 import * as _pluginN from 'eslint-plugin-n';
-export const pluginN = interopDefault(_pluginN);
-
 import * as _pluginPerfectionist from 'eslint-plugin-perfectionist';
-export const pluginPerfectionist = interopDefault(_pluginPerfectionist);
-
-import * as _pluginPrettier from 'eslint-plugin-prettier';
-export const pluginPrettier = interopDefault(_pluginPrettier);
-
-/* @ts-expect-error missing types */
-import * as _configPrettier from 'eslint-config-prettier';
-export const configPrettier = interopDefault(_configPrettier);
-
 import * as _pluginRegexp from 'eslint-plugin-regexp';
-export const pluginRegexp = interopDefault(_pluginRegexp);
-
 import * as _pluginTypeScript from '@typescript-eslint/eslint-plugin';
-export const pluginTypeScript = interopDefault(_pluginTypeScript);
-
-/* @ts-expect-error missing types */
 import * as _pluginUnicorn from 'eslint-plugin-unicorn';
-export const pluginUnicorn = interopDefault(_pluginUnicorn);
-
 import * as _pluginUnocss from '@unocss/eslint-plugin';
-export const pluginUnocss = interopDefault(_pluginUnocss);
-
 /* @ts-expect-error missing types */
 import * as _pluginVue from 'eslint-plugin-vue';
-export const pluginVue = interopDefault(_pluginVue);
-
 /* @ts-expect-error missing types */
 import * as _pluginVueScopedCss from 'eslint-plugin-vue-scoped-css';
-export const pluginVueScopedCss = interopDefault(_pluginVueScopedCss);
-
 /* @ts-expect-error missing types */
 import * as _pluginNuxt2 from 'eslint-plugin-nuxt';
-export const pluginNuxt2 = interopDefault(_pluginNuxt2);
-
 import * as _pluginNuxt3 from '@nuxt/eslint-plugin';
-export const pluginNuxt3 = interopDefault(_pluginNuxt3);
-
 /* @ts-expect-error missing types */
 import * as _pluginReact from 'eslint-plugin-react';
-export const pluginReact = interopDefault(_pluginReact);
-
 /* @ts-expect-error missing types */
 import * as _pluginReactHooks from 'eslint-plugin-react-hooks';
-export const pluginReactHooks = interopDefault(_pluginReactHooks);
-
 /* @ts-expect-error missing types */
 import * as _pluginReactPerf from 'eslint-plugin-react-perf';
-export const pluginReactPerf = interopDefault(_pluginReactPerf);
-
 /* @ts-expect-error missing types */
 import * as _pluginReactRefresh from 'eslint-plugin-react-refresh';
-export const pluginReactRefresh = interopDefault(_pluginReactRefresh);
-
 /* @ts-expect-error missing types */
 import * as _pluginReactNative from 'eslint-plugin-react-native';
-export const pluginReactNative = interopDefault(_pluginReactNative);
-
 /* @ts-expect-error missing types */
 import * as _pluginNext from '@next/eslint-plugin-next';
-export const pluginNext = interopDefault(_pluginNext);
-
 import * as _pluginSolid from 'eslint-plugin-solid';
-export const pluginSolid = interopDefault(_pluginSolid);
-
 /* @ts-expect-error missing types */
 import * as _pluginTailwindcss from 'eslint-plugin-tailwindcss';
-export const pluginTailwindcss = interopDefault(_pluginTailwindcss);
-
 import * as _pluginYml from 'eslint-plugin-yml';
-export const pluginYml = interopDefault(_pluginYml);
-
 /* @ts-expect-error missing types */
 import * as _parserBabel from '@babel/eslint-parser';
-export const parserBabel = interopDefault(_parserBabel);
-
 import * as _parserJsonc from 'jsonc-eslint-parser';
-export const parserJsonc = interopDefault(_parserJsonc);
-
 import * as _parserTypeScript from '@typescript-eslint/parser';
-export const parserTypeScript = interopDefault(_parserTypeScript);
-
 import * as _parserVue from 'vue-eslint-parser';
-export const parserVue = interopDefault(_parserVue);
-
 import * as _parserYml from 'yaml-eslint-parser';
+import { interopDefault } from '../utils';
+
+export const configGitignore = interopDefault(_configGitignore);
+export const pluginImportX = interopDefault(_pluginImportX);
+export const pluginJsdoc = interopDefault(_pluginJsdoc);
+export const pluginJsonc = interopDefault(_pluginJsonc);
+export const pluginMarkdown = interopDefault(_pluginMarkdown);
+export const pluginN = interopDefault(_pluginN);
+export const pluginPerfectionist = interopDefault(_pluginPerfectionist);
+export const pluginRegexp = interopDefault(_pluginRegexp);
+export const pluginTypeScript = interopDefault(_pluginTypeScript);
+export const pluginUnicorn = interopDefault(_pluginUnicorn);
+export const pluginUnocss = interopDefault(_pluginUnocss);
+export const pluginVue = interopDefault(_pluginVue);
+export const pluginVueScopedCss = interopDefault(_pluginVueScopedCss);
+export const pluginNuxt2 = interopDefault(_pluginNuxt2);
+export const pluginNuxt3 = interopDefault(_pluginNuxt3);
+export const pluginReact = interopDefault(_pluginReact);
+export const pluginReactHooks = interopDefault(_pluginReactHooks);
+export const pluginReactPerf = interopDefault(_pluginReactPerf);
+export const pluginReactRefresh = interopDefault(_pluginReactRefresh);
+export const pluginReactNative = interopDefault(_pluginReactNative);
+export const pluginNext = interopDefault(_pluginNext);
+export const pluginSolid = interopDefault(_pluginSolid);
+export const pluginTailwindcss = interopDefault(_pluginTailwindcss);
+export const pluginYml = interopDefault(_pluginYml);
+export const parserBabel = interopDefault(_parserBabel);
+export const parserJsonc = interopDefault(_parserJsonc);
+export const parserTypeScript = interopDefault(_parserTypeScript);
+export const parserVue = interopDefault(_parserVue);
 export const parserYml = interopDefault(_parserYml);
