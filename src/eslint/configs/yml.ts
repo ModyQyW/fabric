53d5fe1687119c0cf013e769dee4aff06990726d
Rui Wu <wurui.dev@gmail.com>
2024-07-26 10:43:53 +0800

    feat(eslint): update yml rules

--- src/eslint/configs/yml.ts
+++ src/eslint/configs/yml.ts
@@ -11,11 +11,28 @@ export function yml(options: YmlOptions = {}): Config[] {
         parser: parserYml,
       },
       plugins: {
+        // @ts-expect-error not matched
         yml: pluginYml,
       },
       rules: {
-        ...pluginYml.configs.standard.rules,
-        ...pluginYml.configs.prettier.rules,
+        // https://github.com/ota-meshi/eslint-plugin-yml/blob/v1.14.0/src/configs/base.ts
+        'no-irregular-whitespace': 'off',
+        'no-unused-vars': 'off',
+        'spaced-comment': 'off',
+
+        // https://github.com/ota-meshi/eslint-plugin-yml/blob/v1.14.0/src/configs/standard.ts
+        'yml/block-mapping': 'error',
+        'yml/block-sequence': 'error',
+        'yml/no-empty-document': 'error',
+        'yml/no-empty-key': 'error',
+        'yml/no-empty-mapping-value': 'error',
+        'yml/no-empty-sequence-entry': 'error',
+        'yml/no-irregular-whitespace': 'error',
+        'yml/no-tab-indent': 'error',
+        'yml/plain-scalar': 'error',
+        'yml/spaced-comment': 'error',
+        'yml/vue-custom-block/no-parsing-error': 'error',
+
         ...rules,
       },
     },
