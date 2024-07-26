58831f9b4ccc05a4cce1959a4e08742703ab2b36
Rui Wu <wurui.dev@gmail.com>
2024-07-26 11:07:58 +0800

    fix(eslint): update yml rules, comment prettier-conflicted rules

--- src/eslint/configs/yml.ts
+++ src/eslint/configs/yml.ts
@@ -21,8 +21,16 @@ export function yml(options: YmlOptions = {}): Config[] {
         'spaced-comment': 'off',
 
         // https://github.com/ota-meshi/eslint-plugin-yml/blob/v1.14.0/src/configs/standard.ts
+        // 'yml/block-mapping-question-indicator-newline': 'error',
         'yml/block-mapping': 'error',
+        // 'yml/block-sequence-hyphen-indicator-newline': 'error',
         'yml/block-sequence': 'error',
+        // 'yml/flow-mapping-curly-newline': 'error',
+        // 'yml/flow-mapping-curly-spacing': 'error',
+        // 'yml/flow-sequence-bracket-newline': 'error',
+        // 'yml/flow-sequence-bracket-spacing': 'error',
+        // 'yml/indent': 'error',
+        // 'yml/key-spacing': 'error',
         'yml/no-empty-document': 'error',
         'yml/no-empty-key': 'error',
         'yml/no-empty-mapping-value': 'error',
@@ -30,6 +38,7 @@ export function yml(options: YmlOptions = {}): Config[] {
         'yml/no-irregular-whitespace': 'error',
         'yml/no-tab-indent': 'error',
         'yml/plain-scalar': 'error',
+        // 'yml/quotes': 'error',
         'yml/spaced-comment': 'error',
         'yml/vue-custom-block/no-parsing-error': 'error',
 
