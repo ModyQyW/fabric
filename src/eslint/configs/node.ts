50f967a5c6c9bc0948a71e373c430694ba4757fc
Rui Wu <wurui.dev@gmail.com>
2024-07-31 10:56:56 +0800

    feat(eslint): update node rules

--- src/eslint/configs/node.ts
+++ src/eslint/configs/node.ts
@@ -6,44 +6,29 @@ export function node(options: NodeOptions = {}): Config[] {
   const { files = [GLOB_SCRIPT, GLOB_VUE], rules = {} } = options;
   return [
     {
+      name: 'node',
       files,
       plugins: {
         n: pluginN,
       },
       rules: {
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
+        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.10.1/lib/configs/_commons.js
         'n/no-deprecated-api': 'error',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
-        'n/no-exports-assign': 'error',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
         'n/no-extraneous-import': 'error',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
         'n/no-extraneous-require': 'error',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
-        // handle by eslint-plugin-import-x
-        // 'n/no-missing-import': 'off',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
-        // handle by eslint-plugin-import-x
-        // 'n/no-missing-require': 'off',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
+        'n/no-exports-assign': 'error',
+        // 'n/no-missing-import': 'error', // Handled by eslint-plugin-import-x.
+        // 'n/no-missing-require': 'error', // Handled by eslint-plugin-import-x.
         'n/no-process-exit': 'error',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
         'n/no-unpublished-bin': 'error',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
         'n/no-unpublished-import': 'error',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
         'n/no-unpublished-require': 'error',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
-        // 'n/no-unsupported-features/es-builtins': 'off',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
-        // 'n/no-unsupported-features/es-syntax': 'off',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
-        // 'n/no-unsupported-features/node-builtins': 'off',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
+        // 'n/no-unsupported-features/es-builtins': 'error',
+        // 'n/no-unsupported-features/es-syntax': 'error',
+        // 'n/no-unsupported-features/node-builtins': 'error',
         'n/process-exit-as-throw': 'error',
-        // https://github.com/eslint-community/eslint-plugin-n/blob/v17.9.0/lib/configs/_commons.js
-        // allow source code to use hashbang for building
-        // 'n/hashbang': 'off',
+        // 'n/hashbang': 'error', // Allow source code to use hashbang for building.
+
         ...rules,
       },
     },
