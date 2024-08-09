8bc710ba71e6d3d5862fdcbd2c6e67d235632b61
Rui Wu <wurui.dev@gmail.com>
2024-08-09 16:32:13 +0800

    feat(eslint): update react rules

--- src/eslint/configs/react.ts
+++ src/eslint/configs/react.ts
@@ -1,5 +1,5 @@
 import { GLOB_JSX, GLOB_TSX } from '../../constants';
-import { hasVite } from '../../env';
+import { hasVite, hasRemix } from '../../env';
 import {
   pluginReact,
   pluginReactHooks,
@@ -25,10 +25,27 @@ export function react(options: ReactOptions = {}): Config[] {
         'react-refresh': pluginReactRefresh,
       },
       rules: {
-        ...pluginReact.configs.recommended.rules,
-        ...pluginReact.configs['jsx-runtime'].rules,
-        ...pluginReactHooks.configs.recommended.rules,
-        ...pluginReactPerf.configs.recommended.rules,
+        // https://github.com/jsx-eslint/eslint-plugin-react/blob/v7.35.0/index.js
+        'react/display-name': 'error',
+        'react/jsx-key': 'error',
+        'react/jsx-no-comment-textnodes': 'error',
+        'react/jsx-no-duplicate-props': 'error',
+        'react/jsx-no-target-blank': 'error',
+        'react/jsx-no-undef': 'error',
+        'react/jsx-uses-vars': 'error',
+        'react/no-children-prop': 'error',
+        'react/no-danger-with-children': 'error',
+        'react/no-deprecated': 'error',
+        'react/no-direct-mutation-state': 'error',
+        'react/no-find-dom-node': 'error',
+        'react/no-is-mounted': 'error',
+        'react/no-render-return-value': 'error',
+        'react/no-string-refs': 'error',
+        'react/no-unescaped-entities': 'error',
+        'react/no-unknown-property': 'error',
+        'react/no-unsafe': 0,
+        'react/prop-types': 'error',
+        'react/require-render-return': 'error',
         'react/sort-comp': [
           'error',
           {
@@ -98,10 +115,29 @@ export function react(options: ReactOptions = {}): Config[] {
             ],
           },
         ],
+
+        // https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks
+        // eslint-plugin-react-hooks v4.6.2
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+
+        // https://github.com/cvazac/eslint-plugin-react-perf/tree/35e9912004da57880939edd4a1ca202740157c66
+        // eslint-plugin-react-perf v3.3.2
+        'react-perf/jsx-no-new-object-as-prop': 'error',
+        'react-perf/jsx-no-new-array-as-prop': 'error',
+        'react-perf/jsx-no-new-function-as-prop': 'error',
+
+        // https://github.com/ArnaudBarre/eslint-plugin-react-refresh/tree/v0.4.9
         'react-refresh/only-export-components': [
           'warn',
-          { allowConstantExport: hasVite },
+          {
+            allowExportNames: hasRemix
+              ? ['meta', 'links', 'headers', 'loader', 'action']
+              : [],
+            allowConstantExport: hasVite,
+          },
         ],
+
         ...rules,
       },
     },
@@ -109,6 +145,7 @@ export function react(options: ReactOptions = {}): Config[] {
       files: typescriptFiles,
       rules: {
         'react/jsx-no-undef': 'off',
+
         ...typescriptRules,
       },
     },
