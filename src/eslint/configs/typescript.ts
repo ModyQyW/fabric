8811fee4def92044dc683ab392fe939cd7f9b1bd
Rui Wu <wurui.dev@gmail.com>
2024-08-09 16:08:58 +0800

    feat(eslint): update typescript rules

--- src/eslint/configs/typescript.ts
+++ src/eslint/configs/typescript.ts
@@ -9,37 +9,117 @@ export function typescript(options: TypeScriptOptions = {}): Config[] {
       ? [GLOB_DTS, GLOB_TS, GLOB_TSX, GLOB_VUE]
       : [GLOB_DTS, GLOB_TS, GLOB_TSX],
     rules = {},
+    typeCheck = true,
+    parserOptions,
   } = options;
   return [
     {
+      name: 'typescript',
       files,
       languageOptions: {
         parser: parserTypeScript,
+        ...(typeCheck
+          ? {
+              parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+                ...parserOptions,
+              },
+            }
+          : {}),
       },
       plugins: {
+        // @ts-expect-error not matched
         '@typescript-eslint': pluginTypeScript,
       },
       rules: {
-        ...pluginTypeScript.configs['eslint-recommended'].overrides[0].rules,
-        ...pluginTypeScript.configs.recommended.rules,
-        // indicate that the export exists only in the type system, not at runtime
-        // requires project
-        // '@typescript-eslint/consistent-type-imports': 'error',
+        // https://github.com/typescript-eslint/typescript-eslint/blob/v8.0.1/packages/eslint-plugin/src/configs/eslint-recommended-raw.ts
+        'constructor-super': 'off', // ts(2335) & ts(2377)
+        'getter-return': 'off', // ts(2378)
+        'no-const-assign': 'off', // ts(2588)
+        'no-dupe-args': 'off', // ts(2300)
+        'no-dupe-class-members': 'off', // ts(2393) & ts(2300)
+        'no-dupe-keys': 'off', // ts(1117)
+        'no-func-assign': 'off', // ts(2630)
+        'no-import-assign': 'off', // ts(2632) & ts(2540)
+        'no-new-native-nonconstructor': 'off', // ts(7009)
+        'no-obj-calls': 'off', // ts(2349)
+        'no-redeclare': 'off', // ts(2451)
+        'no-setter-return': 'off', // ts(2408)
+        'no-this-before-super': 'off', // ts(2376) & ts(17009)
+        'no-undef': 'off', // ts(2304) & ts(2552)
+        'no-unreachable': 'off', // ts(7027)
+        'no-unsafe-negation': 'off', // ts(2365) & ts(2322) & ts(2358)
+
+        // https://github.com/typescript-eslint/typescript-eslint/blob/v8.0.1/packages/eslint-plugin/src/configs/recommended.ts
+        '@typescript-eslint/ban-ts-comment': 'error',
+        'no-array-constructor': 'off',
+        '@typescript-eslint/no-array-constructor': 'error',
+        '@typescript-eslint/no-duplicate-enum-values': 'error',
+        '@typescript-eslint/no-empty-object-type': 'error',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-extra-non-null-assertion': 'error',
+        '@typescript-eslint/no-misused-new': 'error',
+        '@typescript-eslint/no-namespace': 'error',
+        '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+        '@typescript-eslint/no-require-imports': 'error',
+        '@typescript-eslint/no-this-alias': 'error',
+        '@typescript-eslint/no-unnecessary-type-constraint': 'error',
+        '@typescript-eslint/no-unsafe-declaration-merging': 'error',
+        '@typescript-eslint/no-unsafe-function-type': 'error',
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': 'error',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-wrapper-object-types': 'error',
+        '@typescript-eslint/prefer-as-const': 'error',
+        '@typescript-eslint/prefer-namespace-keyword': 'error',
+        '@typescript-eslint/triple-slash-reference': 'error',
+
+        // https://github.com/typescript-eslint/typescript-eslint/blob/v8.0.1/packages/eslint-plugin/src/configs/recommended-type-checked-only.ts
+        ...(typeCheck
+          ? {
+              '@typescript-eslint/await-thenable': 'error',
+              '@typescript-eslint/no-array-delete': 'error',
+              '@typescript-eslint/no-base-to-string': 'error',
+              '@typescript-eslint/no-duplicate-type-constituents': 'error',
+              '@typescript-eslint/no-floating-promises': 'error',
+              '@typescript-eslint/no-for-in-array': 'error',
+              'no-implied-eval': 'off',
+              '@typescript-eslint/no-implied-eval': 'error',
+              '@typescript-eslint/no-misused-promises': 'error',
+              '@typescript-eslint/no-redundant-type-constituents': 'error',
+              '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+              '@typescript-eslint/no-unsafe-argument': 'error',
+              '@typescript-eslint/no-unsafe-assignment': 'error',
+              '@typescript-eslint/no-unsafe-call': 'error',
+              '@typescript-eslint/no-unsafe-enum-comparison': 'error',
+              '@typescript-eslint/no-unsafe-member-access': 'error',
+              '@typescript-eslint/no-unsafe-return': 'error',
+              '@typescript-eslint/no-unsafe-unary-minus': 'error',
+              'no-throw-literal': 'off',
+              '@typescript-eslint/only-throw-error': 'error',
+              'prefer-promise-reject-errors': 'off',
+              '@typescript-eslint/prefer-promise-reject-errors': 'error',
+              'require-await': 'off',
+              '@typescript-eslint/require-await': 'error',
+              '@typescript-eslint/restrict-plus-operands': 'error',
+              '@typescript-eslint/restrict-template-expressions': 'error',
+              '@typescript-eslint/unbound-method': 'error',
+            }
+          : {}),
+
+        // https://typescript-eslint.io/rules/consistent-type-imports/
+        '@typescript-eslint/consistent-type-imports': 'error',
+        // https://typescript-eslint.io/rules/method-signature-style/
         // https://www.totaltypescript.com/method-shorthand-syntax-considered-harmful
         '@typescript-eslint/method-signature-style': 'error',
-        // too ideal for business
-        '@typescript-eslint/no-empty-function': 'off',
-        // too ideal for business
-        '@typescript-eslint/no-empty-interface': 'off',
-        // too ideal for business
-        '@typescript-eslint/no-explicit-any': 'off',
-        // remove type side effects
+        // https://typescript-eslint.io/rules/no-import-type-side-effects/
         '@typescript-eslint/no-import-type-side-effects': 'off',
-        // better to always declare variables and functions before using them
-        '@typescript-eslint/no-use-before-define': 'error',
+        // https://typescript-eslint.io/rules/no-use-before-define/
         'no-use-before-define': 'off',
-        // allow for prototyping
-        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-use-before-define': 'warn',
+
         ...rules,
       },
     },
