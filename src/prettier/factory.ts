17fe67a5adc7393f11aad85375a4dc75a4809a82
Rui Wu <wurui.dev@gmail.com>
2024-08-13 18:41:45 +0800

    feat(prettier)!: only keep prettier-plugin-jsdoc

--- src/prettier/factory.ts
+++ src/prettier/factory.ts
@@ -5,26 +5,10 @@ export function prettier(
   options: Options = {},
   userConfig: Config = {},
 ): Config {
-  const {
-    cssOrder,
-    ianvsSortImports,
-    jsdoc,
-    organizeAttributes,
-    organizeImports,
-    packageJson,
-    tailwindcss,
-    trivagoSortImports,
-  } = parseOptions(options);
+  const { jsdoc } = parseOptions(options);
 
   const plugins = [];
-  if (cssOrder) plugins.push('prettier-plugin-css-order');
-  if (ianvsSortImports) plugins.push('@ianvs/prettier-plugin-sort-imports');
   if (jsdoc) plugins.push('prettier-plugin-jsdoc');
-  if (organizeAttributes) plugins.push('prettier-plugin-organize-attributes');
-  if (organizeImports) plugins.push('prettier-plugin-organize-imports');
-  if (packageJson) plugins.push('prettier-plugin-packagejson');
-  if (trivagoSortImports) plugins.push('@trivago/prettier-plugin-sort-imports');
-  if (tailwindcss) plugins.push('prettier-plugin-tailwindcss');
 
   return {
     plugins,
--- src/prettier/types.ts
+++ src/prettier/types.ts
@@ -4,66 +4,9 @@ export type Config = PrettierConfig & Record<string, any>;
 
 export interface Options {
   /**
-   * Sort CSS or SCSS declarations. Use prettier-plugin-css-order.
-   *
-   * Conflicts with stylelint-config-recess-order.
-   *
-   * @default false
-   */
-  cssOrder?: boolean;
-  /**
-   * Sort import declarations. Use @ianvs/prettier-plugin-sort-imports.
-   *
-   * Conflicts with trivago/prettier-plugin-sort-imports,
-   * prettier-plugin-organize-imports and eslint-plugin-perfectionist.
-   *
-   * @default false
-   */
-  ianvsSortImports?: boolean;
-  /**
    * Format JSDoc and TSDoc comments. Use prettier-plugin-jsdoc.
    *
    * @default true
    */
   jsdoc?: boolean;
-  /**
-   * Organize HTML / Vue attributes. Use prettier-plugin-organize-attributes.
-   *
-   * Conflicts with eslint-plugin-perfectionist and eslint-plugin-vue.
-   *
-   * @default false
-   */
-  organizeAttributes?: boolean;
-  /**
-   * Sort import declarations. Use prettier-plugin-organize-imports.
-   *
-   * Conflicts with ianvs/prettier-plugin-sort-imports,
-   * trivago/prettier-plugin-sort-imports, and eslint-plugin-perfectionist.
-   *
-   * @default false
-   */
-  organizeImports?: boolean;
-  /**
-   * Sort package.json. Use prettier-plugin-packagejson.
-   *
-   * @default true
-   */
-  packageJson?: boolean;
-  /**
-   * Sort HTML / Vue class attribute. Use prettier-plugin-tailwindcss.
-   *
-   * Conflicts with eslint-plugin-unocss.
-   *
-   * Enabled by default if you have TailwindCSS installed.
-   */
-  tailwindcss?: boolean;
-  /**
-   * Sort import declarations. Use @trivago/prettier-plugin-sort-imports.
-   *
-   * Conflicts with ianvs/prettier-plugin-sort-imports,
-   * prettier-plugin-organize-imports and eslint-plugin-perfectionist.
-   *
-   * @default false
-   */
-  trivagoSortImports?: boolean;
 }
