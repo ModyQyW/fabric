c8bdfe26655ffd1edbde423397072cd2a20b6e0a
Rui Wu <wurui.dev@gmail.com>
2024-08-19 15:26:31 +0800

    fix(prettier): fix parsing

--- src/prettier/utils.ts
+++ src/prettier/utils.ts
@@ -1,15 +1,7 @@
-import { hasTailwindCss } from "../env";
 import type { Options } from "./types";
 
 export function parseOptions(options: Options = {}): Required<Options> {
   return {
-    cssOrder: options.cssOrder ?? false,
-    ianvsSortImports: options.ianvsSortImports ?? false,
     jsdoc: options.jsdoc ?? true,
-    organizeAttributes: options.organizeAttributes ?? false,
-    organizeImports: options.organizeImports ?? false,
-    packageJson: options.packageJson ?? true,
-    tailwindcss: options.tailwindcss ?? hasTailwindCss,
-    trivagoSortImports: options.trivagoSortImports ?? false,
   };
 }
