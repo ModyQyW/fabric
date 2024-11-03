2da6fb7405932895b931263d32020b84c47c1d8a
Rui Wu <wurui.dev@gmail.com>
2024-11-03 17:26:17 +0800

    chore: bring back prerelease handling

--- .versionrc.cjs
+++ .versionrc.cjs
@@ -1,5 +1,8 @@
 module.exports = {
   scripts: {
     posttag: "git push --follow-tags",
+    // We have to use prerelease here
+    // as commit-and-tag-version won't trigger preversion script in package.json
+    prerelease: 'git-branch-is main && conc "pnpm:lint" "pnpm:type-check"',
   },
 };
