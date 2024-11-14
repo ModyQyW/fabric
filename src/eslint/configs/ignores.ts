2020f8af52087a2fa922b87415c8dfc8af5be7ad
Panpan <wurui.dev@gmail.com>
2024-11-14 19:25:34 +0800

    fix: eslint fix

--- src/eslint/configs/ignores.ts
+++ src/eslint/configs/ignores.ts
@@ -2,6 +2,5 @@ import { GLOB_EXCLUDE } from "../../constants.ts";
 import type { Config, IgnoresOptions } from "../types.ts";
 
 export function ignores(options: IgnoresOptions = {}): Config[] {
-  const { ignores = GLOB_EXCLUDE } = options;
-  return [{ ignores }];
+  return [{ ignores: options.ignores ?? GLOB_EXCLUDE }];
 }
